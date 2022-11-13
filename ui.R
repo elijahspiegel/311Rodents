18e160a86877afdd8db9921a2edd20afb9ddfbb2
elijahspiegel <elispiegel2@gmail.com>
2022-11-13 17:08:21 -0500

    Added shiny dashboard UI

--- ui.R
+++ ui.R
@@ -7,28 +7,30 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+library(shinydashboard)
 
-
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("NYC 311 Rodent Complaints"),
-
-    # Sidebar with a date range input
-    sidebarLayout(
-        sidebarPanel(
-            dateRangeInput("daterange",
-                           "Date Range of Complaints",
-                           start = '2010-01-01',
-                           end = '2022-',
-                           min = '2010-01-01',
-                           max = '2022-11-02',
-                        )
-        ),
-
-        mainPanel(
-            plotOutput("RatMap")
-        )
+dashboardPage(
+  dashboardHeader(title='NYC 311 Rodent Complaints'),
+  
+  dashboardSidebar(
+    sidebarUserPanel('Your Name'),
+    
+    dateRangeInput("daterange",
+                   "Date Range of Complaints",
+                   start = '2010-01-01',
+                   end = '2022-',
+                   min = '2010-01-01',
+                   max = '2022-11-02',
     )
-))
+  ),
+  
+  dashboardBody(
+    tabsetPanel(
+      tabPanel('Map of Complaints', 
+               fluidRow(
+                 plotOutput("RatMap")
+               )),
+    )
+  )
+  
+)
